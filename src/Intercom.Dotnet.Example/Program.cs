b3e10a44a7bbf9e5bca221c1aac9313c4ca507e9
Alex Forbes-Reed <alexreed@outlook.com>
2016-10-20 17:17:13 +0100

    removed personal access token - oops

--- src/Intercom.Dotnet.Example/Program.cs
+++ src/Intercom.Dotnet.Example/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication
 	{
 		public static void Main(string[] args)
 		{
-			var usersClient = new UsersClient(new Authentication("dG9rOmU5N2U0YmEzX2VlMThfNGRjNl84NWJmX2I2ZDM5Yjk2MTk5NzoxOjA="));
+			var usersClient = new UsersClient(new Authentication("<personal-access-token>"));
 			var user = usersClient.View(new User { user_id = args[0] });
 			foreach (var pair in JObject.Parse(JsonConvert.SerializeObject(user)))
 				Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
